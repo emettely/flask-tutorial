c71aa5b7179868d41281f197388841aa959d1883
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-04-30 14:58:36 +0100

    Adding database

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,29 @@
 from flask import Flask, jsonify, request
 import requests
 import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+import config
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+
+# Set debug status
+if config.ENVIRONMENT == 'development':
+    app.config["DEBUG"] = True
+else:
+    app.config["DEBUG"] = False
+
+for key, value in config.DB[config.ENVIRONMENT].items():
+    app.config[key] = value
+db = SQLAlchemy(app)
+
+class ApiRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(80), unique=True, nullable=False)
+    value = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<{self.key}: {self.value}>'
+
 
 @app.route("/")
 def hello():
