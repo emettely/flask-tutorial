21477e0b047739e8c9f7df6cff68ec505d838d09
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-04-30 12:20:12 +0100

    2

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,8 @@
 # flask_app.py
 from flask import Flask, jsonify
+import requests
+import pandas as pd
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -9,4 +12,5 @@ def hello():
 
 @app.route("/api")
 def json_data():
-    return jsonify(something="somethingelse")
+    data = pd.DataFrame(dict(words=[1, 2, 3]))
+    return data.to_json(great='records')
